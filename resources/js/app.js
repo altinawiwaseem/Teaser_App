1cdfee4d31608ccbd9c5afa085bf9ce1c5967ea3
Waseem <wasemm@live.com>
2025-05-29 16:22:36 +0200

    use Alpine.js CDN instead of the bundled version

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,9 @@
 import "./bootstrap";
-import Alpine from "alpinejs";
+/* import Alpine from "alpinejs";
 
 // Initialize Alpine only once
 if (!window.Alpine) {
     window.Alpine = Alpine;
     Alpine.start();
 }
+ */
